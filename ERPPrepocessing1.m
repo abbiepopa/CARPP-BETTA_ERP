a6cc16bfecabbc5798e0ea697a7953ed987e341f
Abbie Popa <abbiepopa@gmail.com>
2016-08-28 11:31:52 -0700

    updated preproc scripts

--- ERPPrepocessing1.m
+++ ERPPrepocessing1.m
@@ -3,6 +3,9 @@ EEG = pop_resample(EEG, 500);
 
 %Add channel location
 EEG=pop_chanedit(EEG,'lookup', '/Users/abbiepopa/Documents/MATLAB/eeglab13_4_4b/plugins/dipfit2.3/standard_BESA/standard-10-5-cap385.elp');
+%EEG=pop_chanedit(EEG,'lookup', '/Users/minderplab/Desktop/EEGlab/eeglab13_5_4b/plugins/dipfit2.3/standard_BESA/standard-10-5-cap385.elp');
+%EEG=pop_chanedit(EEG,'lookup', 'C:\Users\MIND\Documents\MATLAB\eeglab13_4_4b\plugins\dipfit2.3\standard_BESA\standard-10-5-cap385.elp');
+%EEG=pop_chanedit(EEG,'lookup', '/Users/MIND/Documents/MATLAB/eeglab13_4_4b/plugins/dipfit2.3/standard_BESA/standard-10-5-cap385.elp');
 
 %Filter, High pass 0.1, Low pass 30
 EEG  = pop_basicfilter( EEG,  1:34 , 'Boundary', 'boundary', 'Cutoff', [ 0.1 30], 'Design', 'butter', 'Filter', 'bandpass', 'Order',  2,...
@@ -10,4 +13,4 @@ EEG  = pop_basicfilter( EEG,  1:34 , 'Boundary', 'boundary', 'Cutoff', [ 0.1 30]
 
 %Notch Filter
 EEG  = pop_basicfilter( EEG,  1:34 , 'Boundary', 'boundary', 'Cutoff',  60, 'Design', 'notch', 'Filter', 'PMnotch', 'Order',  180, 'RemoveDC',...
- 'on' ); 
+ 'on' );
