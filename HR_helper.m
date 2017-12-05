c20d3a5d4a6f343a153a6bba4d97903c7268ef2d
Abbie Popa <ampopa@ucdavis.edu>
2017-12-05 10:44:55 -0800

    HR preprocessing

--- HR_helper.m
+++ HR_helper.m
@@ -1,6 +1,23 @@
-pid = '990';
-task = 'Resting_State';
+%% set variable
+% eeglab
+
+pid = '847';
+task = 'eDT_angry';
 current_folder = pwd;
+
+%% filter it
+EEG  = pop_basicfilter( EEG,  1 , 'Boundary', 'boundary', 'Cutoff',  1, 'Design', 'butter', 'Filter', 'highpass', 'Order',  2, 'RemoveDC',...
+ 'on' );
+EEG  = pop_basicfilter( EEG,  1 , 'Boundary', 'boundary', 'Cutoff',  60, 'Design', 'notch', 'Filter', 'PMnotch', 'Order',  180, 'RemoveDC',...
+ 'on' );
+
+filename_filt = strcat(task, '_', pid, '_HR_filt_1hz.set')
+
+EEG = pop_saveset(EEG, 'filename', filename_filt, 'filepath', current_folder);
+
+
+%% it's been filtered
+
 
 filename0 = strcat(task, '_', pid, '_HR_filt_1hz.set')
 
@@ -8,12 +25,12 @@ EEG = pop_loadset('filename', filename0, 'filepath', current_folder);
 eeglab redraw
 %% optional, remove continuous artifacts
 
-EEG = pop_continuousartdet( EEG , 'ampth', [ -500 500], 'chanArray',  1, 'colorseg', [ 1 0.9765 0.5294], 'forder',  100, 'stepms',  250,...
+EEG = pop_continuousartdet( EEG , 'ampth', [ -810 810], 'chanArray',  1, 'colorseg', [ 1 0.9765 0.5294], 'forder',  100, 'stepms',  250,...
  'winms',  500 );
 eeglab redraw
 
 %% add event codes
-thresh = 150
+thresh = 350
 
 EEG = pop_insertcodeatTTL( EEG,1, 'NewCode',  88, 'RelationalOperation', '>=', 'Threshold',  thresh );
 
