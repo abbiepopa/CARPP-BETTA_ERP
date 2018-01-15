fa2128b2d812819a5b9e3cfa0a2cc8818af12dfc
Abbie Popa <ampopa@ucdavis.edu>
2018-01-15 11:21:27 -0800

    automatic areas

--- DT_AUC.m
+++ DT_AUC.m
@@ -1,16 +1,16 @@
 %% File info
 
-file_in = strcat(pwd, '/DT_TD_180111.txt');
+file_in = strcat(pwd, '/DT_22q_180111.txt');
 
 %% N2PC
-file_out = strcat(pwd, '/DT_TD_N2PC_180111.txt');
+file_out = strcat(pwd, '/DT_22q_N2PC_180111.txt');
 
 ALLERP = pop_geterpvalues( file_in, [ 150 450],  3,...
   35 , 'Baseline', 'pre', 'Binlabel', 'on', 'FileFormat', 'long', 'Filename', file_out,...
  'Fracreplace', 'NaN', 'InterpFactor',  1, 'Measure', 'arean', 'PeakOnset',  1, 'Resolution',  3 );
 
 %% Pd
-file_out = strcat(pwd, '/DT_TD_Pd_180111.txt');
+file_out = strcat(pwd, '/DT_22q_Pd_180111.txt');
 
 ALLERP = pop_geterpvalues( file_in, [ 200 500],  3,...
   35 , 'Baseline', 'pre', 'Binlabel', 'on', 'FileFormat', 'long', 'Filename', file_out,...
